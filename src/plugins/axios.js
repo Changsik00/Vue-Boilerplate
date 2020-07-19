c3fb421c8a053f71d5941a9cc1953ab4236d062e
Changsik00 <lowmans00@gmail.com>
2020-07-19 17:08:41 +0900

    axios 에서 showSnackbar 방식 변경

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,38 +1,37 @@
 // https://github.com/axios/axios
 // https://github.com/imcvampire/vue-axios
 
-import Vue from 'vue';
-import axios from 'axios';
-import store from '../store'
+import Vue from "vue";
+import axios from "axios";
+import store from "../store";
 
 Vue.prototype.$axios = axios;
 
 axios.defaults.timeout = 5000;
 axios.defaults.baseURL = "https://reqres.in/api/";
 
-function axisoErrorHandler (error) {
-    console.log("#@# Axios error", error)
-    store.dispatch("showSnackbar", error);
-    store.dispatch("hideLoading");
-    return Promise.reject(error.response);
+function axisoErrorHandler(error) {
+  store.dispatch("hideLoading");
+  Vue.prototype.$showSnackbar(error);
+  return Promise.reject(error.response);
 }
 
 axios.interceptors.request.use(
-    config => {
-        store.dispatch("showLoading");
-        return config;
-    },
-    error => {
-        return axisoErrorHandler(error);
-    }
+  (config) => {
+    store.dispatch("showLoading");
+    return config;
+  },
+  (error) => {
+    return axisoErrorHandler(error);
+  }
 );
 
 axios.interceptors.response.use(
-    response => {
-        store.dispatch("hideLoading");
-        return response;
-    },
-    error => {
-        return axisoErrorHandler(error);
-    }
+  (response) => {
+    store.dispatch("hideLoading");
+    return response;
+  },
+  (error) => {
+    return axisoErrorHandler(error);
+  }
 );
